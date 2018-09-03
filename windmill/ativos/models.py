d57178c06610a8db3261dff750e8876a83aefd5a
AnimaTakeshi <33721822+AnimaTakeshi@users.noreply.github.com>
2018-09-03 18:42:27 -0300

    Commit inicial

---
+++ windmill/ativos/models.py
@@ -0,0 +1,35 @@
+import datetime
+from django.db import models
+
+# Create your models here.
+class Ativo(models.Model):
+
+    TIPOS_DE_ATIVOS = (
+        ('RV', 'Ações'),
+        ('RF', 'Renda Fixa'),
+        ('RE', 'Real Estate'),
+        ('FRV', 'Fundo de Renda Variável'),
+        ('FRF', 'Fundo de Renda Fixa'),
+        ('FII', 'Fundo Imobiliário'),
+    )
+
+    nome = models.CharField(max_length=25, unique=True)
+    bbg_ticker = models.CharField(max_length=25, unique=True)
+    tipo_ativo = models.CharField(max_length=5, choices=TIPOS_DE_ATIVOS,
+    default='RV')
+    pais = models.ForeignKey('Pais', on_delete=models.PROTECT)
+
+    class Meta:
+        abstract = True
+        ordering = ['nome']
+
+class Pais(models.Model):
+    nome = models.CharField(max_length=20, unique=True)
+    moeda = models.ForeignKey('Moeda', on_delete=models.PROTECT)
+
+class Moeda(models.Model):
+    nome = models.CharField(max_length=15, unique=True)
+    codigo = models.CharField(max_length=3, unique=True)
+
+class Renda_Fixa(Ativo):
+    vencimento = models.DateField(default=datetime.date.max)
